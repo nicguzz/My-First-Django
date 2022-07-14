2cd9036cd7dc27e52af2b62872a04f57a55ab5be
Nic <85948162+nicguzz@users.noreply.github.com>
2022-07-14 17:37:04 +0200

    Delete my_first_django directory

--- my_first_django/my_first_django/my_first/form.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Order
-
-class TransacaoForm(ModelForm):
-    class Meta:
-        model = Order
-        fields = ['id', 'first_name', 'last_name', 'age', 'meal']
